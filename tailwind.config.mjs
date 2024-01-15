239bbba573a9688c6b679c8434c73827e5962580
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-01-15 20:45:04 -0300

    Migre clase header y nav a tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#006878",
+        secondary: "#dc0054",
+      },
+      spacing: {
+        header: "4rem",
+      },
+    },
+  },
+  plugins: [],
+};

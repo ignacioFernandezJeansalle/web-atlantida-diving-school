99811acacf49307c3871ca7f9f9efaddd55aedf8
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-01-17 19:25:01 -0300

    Cree componente HeaderNavLink y pase a tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,10 @@ export default {
       colors: {
         primary: "#006878",
         secondary: "#dc0054",
+        tcligth: "#fafafa",
+        "tcligth-h": "#e5e5e5",
+        tcdark: "#fafafa",
+        "tcdark-h": "#e5e5e5",
       },
       spacing: {
         navbar: "4rem",

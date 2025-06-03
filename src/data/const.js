4aa0ebd76970784552b191f0247aba1fa2b2a7ff
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2025-06-03 16:19:15 -0300

    Creacion archivo const y actualizacion de info en las constantes

---
+++ src/data/const.js
@@ -0,0 +1,12 @@
+const carouselData = [
+  `<h2 class="item-carousel--title">EXPERIENCIA</h2>
+     <p class="item-carousel--description">Lo más importante al elegir un curso de buceo es la experiencia de los Instructores.<br />Desde 1986, <span class="yearsSince1986"></span> años ininterrumpidos dictando cursos de buceo. Más de 7950 alumnos certificados.</p>`,
+  `<h2 class="item-carousel--title">DISPONIBILIDAD</h2>
+     <p class="item-carousel--description">Programá el curso de buceo según tu disponibilidad. Con nosotros podés elegir día, horario y cantidad de clases semanales. Lunes a Sábados.</p>`,
+  `<h2 class="item-carousel--title">COMODIDAD</h2>
+     <p class="item-carousel--description">Cursos de buceo totalmente personalizados de 1 a 4 alumnos como máximo. Nuestro objetivo es tu comodidad y que te sientas seguro y sin presiones. Siempre hay un instructor para vos.</p>`,
+  /*`<h2 class="item-carousel--title">PROMOCION</h2>
+     <p class="item-carousel--description">Inscribite AHORA con un 2x1. Uno paga y dos hacen el curso de buceo Open Water Diver.</p>`,*/
+];
+
+export { carouselData };
